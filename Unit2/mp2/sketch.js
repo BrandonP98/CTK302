382d20e050d0002a911f3ea25ab80fea191d9f6a
brandonp98 <bcpullu@ilstu.edu>
2021-10-06 23:54:26 -0500

    3

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -14,7 +14,7 @@ function setup() {
 function draw() {
   clear();
   numberOfTouches = touches.length;
-  text(numberOfTouches + ' touches', 5, 10);
+  //text(numberOfTouches + 'touches', 5, 10);
 
   switch(numberOfTouches) {
     case 0:
@@ -26,7 +26,7 @@ function draw() {
       case 1:
       image(skyplane,  0, 0, 500, 430) ;
       image(plane, x, 100, 100, 100) ;
-      text("So he decides to book a flight somewhere", 5, 22) ;
+      text("So he decides to book a flight somewhere", 20, 22, 460, 400) ;
       //rect(x, 100, 100, 100) ;
       x = x + 3 ;
       if (x > width) {
@@ -37,7 +37,7 @@ function draw() {
       case 2:
       image(artdecomiami, 0, 0, 500, 430) ;
       fill('black')
-      text("Which ends up flying him out to sunny and coastal Miami where he soaks up South Beach", 5, 22) ;
+      text("Which ends up flying him out to sunny and coastal Miami where he soaks up South Beach", 20, 22, 460, 400) ;
       break ;
 
       //case 3:
