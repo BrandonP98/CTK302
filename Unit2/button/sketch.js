1362bb4b64ce8b55edbe0fe24a23dc966628cc49
BrandonP98 <77593269+BrandonP98@users.noreply.github.com>
2021-10-11 22:33:56 -0500

    Add files via upload

---
+++ Unit2/button/sketch.js
@@ -0,0 +1,48 @@
+let state = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+
+  background('red');
+
+  switch (state) {
+
+    case 0:
+      fill('green')
+      rect(135, 110, 100, 100)
+      fill('black')
+      text ("Christmas season is approaching!", 100, 80)
+
+      break;
+
+    case 1:
+      background('green');
+      fill('red')
+      rect(135, 110, 100, 100)
+      fill('black')
+      text ("Christmas season is now here!", 110, 80)
+
+      break;
+
+    //case 2:
+      //text("2", 100, 100);
+      //break;
+
+  }
+  fill('black')
+  text(mouseX + ", " + mouseY, 15, 15)
+}
+
+
+function mouseReleased() {
+  if ((state == 0) && (mouseX > 135) && (mouseX < 235) && (mouseY > 110) && (mouseY < 210)) {
+    state = 1;
+    }
+  else
+  if ((state == 1) && (mouseX > 135) && (mouseX < 235) && (mouseY > 110) && (mouseY < 210)) {
+    state = 0;
+    }
+}
