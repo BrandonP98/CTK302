4d51d667357bf35a2eb626cb728060a5b927959c
brandonp98 <bcpullu@ilstu.edu>
2021-10-13 00:10:21 -0500

    10

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -7,29 +7,63 @@ function setup() {
 function draw() {
   clear();
   numberOfTouches = touches.length;
-  text(numberOfTouches + ' touches', 5, 10);
+  //text(numberOfTouches + ' touches', 5, 10);
 
   switch(numberOfTouches) {
     case 0:
-      text("no one is touching the screen", 5, 22) ;
+      textSize(20)
+      fill('black')
+      text("I bet", 5, 22) ;
+
       break ;
 
       case 1:
-       text("it's kind of lonely here", 5, 22) ;
-      // put a picture here
+      textSize(20)
+      text("you can't", 5, 22) ;
+      fill('#f0bdbd') ;
+      triangle(40, 85, 58, 20, 86, 75);
       break ;
 
       case 2:
-      text("two fingers are touching the screen", 5, 22) ;
+      textSize(20)
+      text("fit", 5, 22) ;
+      fill('#f0e1bd');
+      rect(90, 60, 300, 45) ;
             // put a picture here
       break ;
 
       case 3:
-     text("three are touching the screen", 5, 22) ;
+      textSize(20)
+      text("6 fingers", 5, 22) ;
+      fill('#bdc5f0')
+      ellipse(252, 144, 72, 72);
             // put a picture here
       break ;
 
+      case 4:
+      textSize(20)
+      text("on the screen", 5, 22) ;
+      fill('#f0efbd');
+      arc(279, 300, 220, 220, PI, TWO_PI);
 
+      break ;
+
+      case 5:
+      textSize(20)
+      text("at once", 5, 22) ;
+      fill('#beeddb')
+      quad(189, 18, 216, 18, 216, 360, 144, 360);
+      break ;
+      // put a picture here
+      break ;
+
+      case 6:
+      textSize(20)
+      text("and still see all the shapes", 5, 22) ;
+      fill('#bdecf0') ;
+      rect(75, 155, 75, 75) ;
+      // put a picture here
+      break ;
   }
 
 }
