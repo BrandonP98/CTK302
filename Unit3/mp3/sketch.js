ee9397c687581d42981f7e206441f01d6c1c5624
brandonp98 <bcpullu@ilstu.edu>
2021-10-28 11:21:40 -0500

    32

--- Unit3/mp3/sketch.js
+++ Unit3/mp3/sketch.js
@@ -48,7 +48,7 @@ function draw() {
     background('red');
     fill('white');
     textSize(70);
-    text("SO YOU CAN SUCCEED?", 400, 300) ;
+    text("SO YOU CAN SUCCEED?", 370, 300) ;
     break ;
 
     case 3:
