666e5a650f27083ed7623417a12bc56c36984cd9
brandonp98 <bcpullu@ilstu.edu>
2021-11-18 10:51:33 -0600

    City

--- Unit4/mp4/sketch.js
+++ Unit4/mp4/sketch.js
@@ -9,10 +9,17 @@ var plane;
 // var bunnyImage;
 var frogPos;
 var city = 0;
+let prevcity = -1;
 var myCityString;
- //1 = Tampa
- //2 = Albuquerque
- //3 = Seattle
+//1 = Tampa
+//2 = Albuquerque
+//3 = Seattle
+var weather;
+var weatherID = 0; // returned in the JSON weather element
+var windspeed = 0 ;
+var temp = 0;
+var humidity = 0;
+var feels_like = 0;
 
 function setup() {
 
@@ -44,8 +51,8 @@ function draw() {
   // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
   //xPosition = map(gamma, -18, 18, 0, width);
   //yPosition = map(beta, 25, 45, 0, height);
-    xPosition = mouseX;
-    yPosition = mouseY;
+  xPosition = mouseX;
+  yPosition = mouseY;
 
   // move the frog around the screen
   push(); // before you use translate, rotate, or scale commands, push and then pop after
@@ -55,75 +62,111 @@ function draw() {
   // draw the FROG
   image(plane, 0, 0, 100, 100);
   pop();
-  if((xPosition < 170) && (yPosition > 250) && (yPosition < 350)){
-  city = 2;
-  myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=Albuquerque,NM,US&units=imperial&';
+
+ prevcity = city;
+  //determine what city we're in
+  if ((xPosition < 170) && (yPosition > 250) && (yPosition < 350)) {
+    city = 2;
+    myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=Albuquerque,NM,US&units=imperial&appid=2ab3fd961cc8c4aacb1786ddb79e8da5';
   }
 
-  switch(city) {
+if (prevcity != city){
+  loadJSON(myCityString, gotData);
+}
+
+prevcity = city;
+  switch (city) {
     case 0:
       //image(artdecometro, 0, 0, 500, 430) ;
-      fill('#b3b3b3') ;
-      text("Plane in Center", width/2, height/2) ;
-      break ;
+      fill('#b3b3b3');
+      text("Plane in Center", width / 2, height / 2);
+      break;
 
-      case 1:
-      image(skyplane,  0, 0, 500, 430) ;
-      image(plane, x, 100, 100, 100) ;
-      text("So he decides to book a flight somewhere", 20, 22, 460, 400) ;
+    case 1:
+    background('green');
+      text("Tampa", 20, 22, 460, 400);
       //rect(x, 100, 100, 100) ;
-      x = x + 3 ;
-      if (x > width) {
-        x = 0 ;
-        }
-      break ;
+      text("What is the weather in " + weather.name + "?", 20, 20);
+     text("Windspeed Is " + windspeed, 20, 40);
+     text("Temperature is " + temp, 20, 60);
+     text("Humidty is " + humidity, 20, 80);
+     text("Feels like " + feels_like, 20, 100);
+      break;
 
-      case 2:
-      image(albuquerque, 0, 0, 500, 430) ;
+    case 2:
+      image(albuquerque, 0, 0, 500, 430);
+      if (weather) {
+
+
       fill('black')
-      text("Now in New Mexico", 20, 22) ;
-      break ;
+      text("What is the weather in " + weather.name + "?", 20, 20);
+     text("Windspeed Is " + windspeed, 20, 40);
+     text("Temperature is " + temp, 20, 60);
+     text("Humidty is " + humidity, 20, 80);
+     text("Feels like " + feels_like, 20, 100);
+            }
+      break;
 
-
-  // Debugging information -- take this out when you're ready for production!
-  // Just a bunch of text commands to display data coming in from addEventListeners
-  //textAlign(LEFT);
-  //textSize(20);
-  //fill('black');
-  //text("orientation data:", 25, 25);
-  //textSize(15);
-  //text("alpha: " + alpha, 25, 50);
-  //text("beta: " + beta, 25, 70);
-  //text("gamma: " + gamma, 25, 90);
-  //textSize(20);
-  //text("acceleration data:", 25, 125);
-  //textSize(15);
-  //text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
-  //text("y = " + y, 25, 170);
-  //stext("z = " + z, 25, 190);
-  text(xPosition + ", " + yPosition, 25, 210);
-
+      case 3:
+          background('yellow');
+        fill('black')
+        text("What is the weather in " + weather.name + "?", 20, 20);
+       text("Windspeed Is " + windspeed, 20, 40);
+       text("Temperature is " + temp, 20, 60);
+       text("Humidty is " + humidity, 20, 80);
+       text("Feels like " + feels_like, 20, 100);
+        break;
 }
 
-function deviceShaken() {
+      // Debugging information -- take this out when you're ready for production!
+      // Just a bunch of text commands to display data coming in from addEventListeners
+      //textAlign(LEFT);
+      //textSize(20);
+      //fill('black');
+      //text("orientation data:", 25, 25);
+      //textSize(15);
+      //text("alpha: " + alpha, 25, 50);
+      //text("beta: " + beta, 25, 70);
+      //text("gamma: " + gamma, 25, 90);
+      //textSize(20);
+      //text("acceleration data:", 25, 125);
+      //textSize(15);
+      //text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
+      //text("y = " + y, 25, 170);
+      //stext("z = " + z, 25, 190);
+      text(xPosition + ", " + yPosition, 25, 210);
 
+  }
+
+  function deviceShaken() {
+
+  }
+
+
+  // HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
+
+  // Read in accelerometer data
+  window.addEventListener('deviceorientation', function(e) {
+    alpha = e.alpha;
+    beta = e.beta;
+    gamma = e.gamma;
+  });
+
+
+  // accelerometer Data
+  window.addEventListener('devicemotion', function(e) {
+    // get accelerometer values
+    x = e.acceleration.x;
+    y = e.acceleration.y;
+    z = e.acceleration.z;
+  });
+
+  function gotData(data) {
+
+  weather = data;
+  console.log(weather); // for debugging purposes, print out the JSON data when we get it.
+  windspeed = weather.wind.speed;
+temp = weather.main.temp ;
+humidity = weather.main.humidity;
+feels_like = weather.main.feels_like;
 }
-
-
-// HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
-
-// Read in accelerometer data
-window.addEventListener('deviceorientation', function(e) {
-  alpha = e.alpha;
-  beta = e.beta;
-  gamma = e.gamma;
-});
-
-
-// accelerometer Data
-window.addEventListener('devicemotion', function(e) {
-  // get accelerometer values
-  x = e.acceleration.x;
-  y = e.acceleration.y;
-  z = e.acceleration.z;
-});
