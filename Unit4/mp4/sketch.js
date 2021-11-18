630733d8fc512f63e0c73ab63a729b919347e7eb
brandonp98 <bcpullu@ilstu.edu>
2021-11-18 10:23:33 -0600

    NM

--- Unit4/mp4/sketch.js
+++ Unit4/mp4/sketch.js
@@ -9,6 +9,7 @@ var plane;
 // var bunnyImage;
 var frogPos;
 var city = 0;
+var myCityString;
  //1 = Tampa
  //2 = Albuquerque
  //3 = Seattle
@@ -28,6 +29,7 @@ function setup() {
 
   // load any images you need
   plane = loadImage("assets/plane.png");
+  albuquerque = loadImage("assets/NewMexico.png");
   imageMode(CENTER);
   rectMode(CENTER);
   noStroke();
@@ -53,38 +55,52 @@ function draw() {
   // draw the FROG
   image(plane, 0, 0, 100, 100);
   pop();
-  //if(xPosition >)
+  if((xPosition < 170) && (yPosition > 250) && (yPosition < 350)){
+  city = 2;
+  myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=Albuquerque,NM,US&units=imperial&';
+  }
 
+  switch(city) {
+    case 0:
+      //image(artdecometro, 0, 0, 500, 430) ;
+      fill('#b3b3b3') ;
+      text("Plane in Center", width/2, height/2) ;
+      break ;
 
-  // update the frog's position using the accelerometer data
-  //frogPos.x = xPosition;
-  //frogPos.y = yPosition;
+      case 1:
+      image(skyplane,  0, 0, 500, 430) ;
+      image(plane, x, 100, 100, 100) ;
+      text("So he decides to book a flight somewhere", 20, 22, 460, 400) ;
+      //rect(x, 100, 100, 100) ;
+      x = x + 3 ;
+      if (x > width) {
+        x = 0 ;
+        }
+      break ;
 
-
-
-  // MORE DECORATIONS - write that pretty ATK type on top.
-  fill('white');
-  textSize(40);
-  textAlign(CENTER);
-  text("", width / 2, 600, windowWidth - 200, windowHeight - 200);
+      case 2:
+      image(albuquerque, 0, 0, 500, 430) ;
+      fill('black')
+      text("Now in New Mexico", 20, 22) ;
+      break ;
 
 
   // Debugging information -- take this out when you're ready for production!
   // Just a bunch of text commands to display data coming in from addEventListeners
-  textAlign(LEFT);
-  textSize(20);
-  fill('black');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y, 25, 170);
-  text("z = " + z, 25, 190);
+  //textAlign(LEFT);
+  //textSize(20);
+  //fill('black');
+  //text("orientation data:", 25, 25);
+  //textSize(15);
+  //text("alpha: " + alpha, 25, 50);
+  //text("beta: " + beta, 25, 70);
+  //text("gamma: " + gamma, 25, 90);
+  //textSize(20);
+  //text("acceleration data:", 25, 125);
+  //textSize(15);
+  //text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
+  //text("y = " + y, 25, 170);
+  //stext("z = " + z, 25, 190);
   text(xPosition + ", " + yPosition, 25, 210);
 
 }
