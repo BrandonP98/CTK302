d60710f6a1ad8e6e174e2b9b702268cc8415cd54
brandonp98 <bcpullu@ilstu.edu>
2021-12-06 13:08:19 -0600

    4

--- Unit4/weathersystem/sketch.js
+++ Unit4/weathersystem/sketch.js
@@ -1,7 +1,75 @@
+// Note - you must change line 19 to your own APPID to get this to work!
+
+var weather;
+var weatherID = 0; // returned in the JSON weather element
+var myState = 0;
+var x = 0;
+var windspeed = 0 ;
+var temp = 0;
+var humidity = 0;
+var feels_like = 0;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
+
+    santacruz = loadImage("assets/santacruz.jpeg");
+
+  // HERE is the call to get the weather.
+
+  var myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=Santa Cruz,CA,US&units=imperial&';
+
+  //You can also use "zipcode" - var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?zip=61820,us&units=imperial&';
+
+  var myIDString = 'appid=2ab3fd961cc8c4aacb1786ddb79e8da5'; // USE YOUR ID HERE, take out the x's!!!
+
+  var myBigString = myCityString + myIDString ;
+
+  loadJSON(myBigString, gotData); // that gotData function happens when JSON comes back.
+
 }
 
-function draw() {
 
+function gotData(data) {
+
+  weather = data;
+  console.log(weather); // for debugging purposes, print out the JSON data when we get it.
+  windspeed = weather.wind.speed;
+temp = weather.main.temp ;
+humidity = weather.main.humidity;
+feels_like = weather.main.feels_like;
+}
+
+
+function draw() {
+  switch (myState) {
+    case 0:
+      if (weather) {
+        myState = 1;
+      }
+      break;
+
+    case 1:
+      background(200);
+      image(santacruz, width /2, height /2, width, height);
+      fill('black');
+      text("What is the weather in " + weather.name + "?", 20, 20);
+      text("Windspeed Is " + windspeed, 20, 40);
+      text("Temperature is " + temp, 20, 60);
+      text("Humidty is " + humidity, 20, 80);
+      text("Feels like " + feels_like, 20, 100);
+
+
+
+      // cloud
+      fill('white');
+      noStroke();
+      ellipse(x, 300, 200, 100);
+
+      // move the cloud's x position
+      x = x + windspeed;
+      if (x > width) x = 0;
+
+      break;
+
+  }
 }
