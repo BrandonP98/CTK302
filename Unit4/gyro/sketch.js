97cf74613c6cb30a0fda36191caf26d8876b38b2
brandonp98 <bcpullu@ilstu.edu>
2021-12-09 15:40:48 -0600

    03

--- Unit4/gyro/sketch.js
+++ Unit4/gyro/sketch.js
@@ -37,7 +37,7 @@ function setup() {
 function draw() {
 
   background('#f0c456'); // light blue
-  image(planet, 60, 70, 290, 290);
+  image(planet, width/2, height/2, 290, 290);
 
   // the map command !!!!
   // takes your variable and maps it from range 1 to range 2
