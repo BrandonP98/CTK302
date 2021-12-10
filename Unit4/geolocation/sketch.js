236687c43f364b721656504379e979c0a99838a0
brandonp98 <bcpullu@ilstu.edu>
2021-12-09 21:12:10 -0600

    10

--- Unit4/geolocation/sketch.js
+++ Unit4/geolocation/sketch.js
@@ -1,20 +1,24 @@
 // Note - you must change line 19 to your own APPID to get this to work!
 
+let locate;
+let kicks;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
     locate = loadImage("assets/location.jpg");
     kicks = loadImage("assets/alleykicks.jpg");
 
+}
+
 function draw() {
 
       background('#ddff87');
-      image(locate, 30, 30, 200, 200);
-      image(kicks, 100, 100, 200, 200);
+      //image(locate, 30, 30, 200, 200);
+      //image(kicks, 100, 100, 200, 200);
       //fill('black');
       textSize(24)
-      text("I enjoy taking walks at night (in somewhat comfortable weather ");
+      text("I enjoy taking walks at night (in somewhat comfortable weather " 30, 300);
 
 
-  }
 }
